5b75b45e4afde84d6412d395f63a2935a73bc7a5
Sakuna Sanka <sakuna@Sakunas-MacBook-Air.local>
2025-07-03 19:01:29 +0530

    [SONA-16] Fix notch issue in profile pages

--- app/(hidden)/profile/profile.tsx
+++ app/(hidden)/profile/profile.tsx
@@ -1,5 +1,5 @@
 import { router } from 'expo-router';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
 
 const handleSignOut = () => {
   // clear tokens or state
@@ -8,7 +8,11 @@ const handleSignOut = () => {
 
 const profile = () => {
     return (
-       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <SafeAreaView style={{ flex: 1 }}>
+        {/* Added padding to avoid notch overlap */}
+        <View className='pt-6' />
+        
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <View style={{ marginBottom: 20 }}>
                 <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Profile</Text>
             </View>
@@ -19,10 +23,11 @@ const profile = () => {
             <Text style={{ marginTop: 20, fontSize: 16, color: '#666' }} >
                 This is the profile page. You can add more features here
             </Text>
-            <TouchableOpacity onPress={() => router.push('../(hidden)/profile/view_profile')} style={{ padding: 10, backgroundColor: '#EF5DA8', borderRadius: 5 }}>
+            <TouchableOpacity onPress={() => router.push('../(hidden)/profile/view_profile')} style={{ padding: 10, backgroundColor: '#EF5DA8', borderRadius: 5, marginTop: 10 }}>
                 <Text style={{ color: '#fff', fontSize: 16 }}>View Profile</Text>
             </TouchableOpacity>
         </View>
+      </SafeAreaView>
     );
 }
 

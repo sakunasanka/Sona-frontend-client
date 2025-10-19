58518d56f1f6b17609f007ef117ec21c8c67477a
Hiruna Pramuditha <hiruna.nishan@gmail.com>
2025-10-19 15:51:50 +0530

    [SONA-73] peofile pages added

--- app/(hidden)/profile/view_profile.tsx
+++ app/(hidden)/profile/view_profile.tsx
@@ -256,6 +256,16 @@ export default function Profile() {
             </View>
             <Text className="text-base text-gray-900">Help & Support</Text>
           </TouchableOpacity>
+
+          <TouchableOpacity 
+            className="flex-row items-center py-4 border-b border-gray-100"
+            onPress={() => router.push('/profile/view_mood')}
+          >
+            <View className="w-9 h-9 rounded-full bg-blue-50 justify-center items-center mr-4">
+              <History size={20} color="#2563EB" />
+            </View>
+            <Text className="text-base text-gray-900">View mood analytics</Text>
+          </TouchableOpacity>
           
           {/* Only show "Apply for Free Student Package" if user is not already a student */}
           {!isStudent && (

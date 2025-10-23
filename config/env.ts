4c4aa456dbb619bb3feb9444adb2f20a420820d9
Hiruna Pramuditha <hiruna.nishan@gmail.com>
2025-10-24 02:43:36 +0530

    [main] changed the credentials from env to local.env

---
+++ config/env.ts
@@ -0,0 +1,6 @@
+export const server_URL = process.env.EXPO_PUBLIC_API_URL;
+export const host = process.env.EXPO_PUBLIC_HOST;
+export const VITE_CLOUDINARY_CLOUD_NAME = process.env.EXPO_PUBLIC_CLOUDINARY_CLOUD_NAME;
+export const VITE_CLOUDINARY_API_KEY = process.env.EXPO_PUBLIC_CLOUDINARY_API_KEY;
+export const VITE_CLOUDINARY_API_SECRET = process.env.EXPO_PUBLIC_CLOUDINARY_API_SECRET;
+export const CLOUDINARY_URL = process.env.EXPO_PUBLIC_CLOUDINARY_URL;
